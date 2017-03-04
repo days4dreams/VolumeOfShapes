0bef63a90ccf96f213afbcb0b8bb60cdb0166073
lauren <lauren@LAURENJANEYBC1C>
2017-03-04 18:35:58 +0000

    Add project files.

---
+++ Volume/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               VolumeCalculator VolumeCalculator = new VolumeCalculator();
+               double side = 2;
+               double length = 3;
+               double height = 4;
+               double width = 5;
+               double baseA = 6;
+               double radius = 7;
+
+                Console.WriteLine("Demonstrating Math in C Sharp");
+                Console.WriteLine("Calculating the volume of a cube.");
+                Console.WriteLine("When the cube's side measures: " + side + "cm");
+
+                double volumeOfCube = 1;
+                volumeOfCube = VolumeCalculator.VolumeOfCube(side);
+                Console.WriteLine("The volume of the cube will be: " + volumeOfCube + "cm2");
+
+                Console.WriteLine("Calculating the volume of a cuboid.");
+                Console.WriteLine("When the cubdoid's length is " + length + "cm, width is " + width + "cm and height is " + height + "cm");
+            
+                double volumeOfCuboid = 1;
+                volumeOfCuboid = VolumeCalculator.VolumeOfCuboid(length, width, height);
+                Console.WriteLine("Volume of cuboid: " + volumeOfCuboid);
+
+                Console.WriteLine("Calculating the volume of a Sphere.");
+                Console.WriteLine("When the sphere's radius is " + length + "cm");
+
+                double volumeOfSphere = 1;
+                volumeOfSphere = VolumeCalculator.VolumeOfSphere(radius);
+                Console.WriteLine("Volume of sphere: " + volumeOfSphere);
+
+
+            Console.ReadKey();
+                
+           
+         }
+
+      }
+}
---
+++ Volume/VolumeCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volume
+{
+    public class VolumeCalculator
+    {
+        public VolumeCalculator()
+        {   }
+
+        public double VolumeOfCube(double s)
+        {
+            double cube = 1;
+            cube = Math.Pow(s, 3);
+            return cube;
+        }
+
+        public double VolumeOfCuboid(double length, double width, double height)
+        {
+            double cuboid = 1;
+            cuboid = length * width * height;
+            return cuboid;
+        }
+
+        public double VolumeOfSphere(double radius)
+        {
+            double sphere = 1;
+            sphere = (4 / 3) * Math.PI * Math.Pow(radius, 3);
+            return sphere;
+        }
+
+        public double VolumeofCylinder(double radius, double height)
+        {
+            double cylinder = 0;
+            cylinder = Math.PI * Math.Pow(radius, 2) * height;
+            return cylinder;
+        }
+
+        public double VolumeOfPrism(double baseA, double height)
+        {
+            double prism = 0;
+            prism = (1 / 2 * baseA * height) * height;
+            return prism;
+        }
+    }
+}
